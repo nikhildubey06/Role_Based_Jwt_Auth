45596f9ebc5ad64f4e7064f994d838499bd50eb0
Nikhil <nikhildubey173231@gmail.com>
2023-08-21 16:44:56 +0530

    second change

--- src/main/java/com/rolebasedauthorization/cotroller/UserController.java
+++ src/main/java/com/rolebasedauthorization/cotroller/UserController.java
@@ -3,6 +3,7 @@ package com.rolebasedauthorization.cotroller;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,11 +30,13 @@ public class UserController {
 	}
 	
     @GetMapping({"/forAdmin"})
+    @PreAuthorize("hasRole('Admin')")
     public String forAdmin(){
         return "This URL is only accessible to the admin";
     }
 
     @GetMapping({"/forUser"})
+    @PreAuthorize("hasRole('Admin')")
     public String forUser(){
         return "This URL is only accessible to the user";
     }
